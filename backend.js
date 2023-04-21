2ce66cf9d66d198b954f4c8a3322c8af6ade37e2
KorPou <korinakatsa@gmail.com>
2023-04-21 09:53:01 +0200

    Js Varieables and Arrays - Bootstrap 5 Fundamentals - Gradients and Keyframes

---
+++ backend.js
@@ -0,0 +1,20 @@
+var student1 = "Mike" //String
+var score = 10 //Integer
+var learningSpeed = 8.2 //Float
+var isLearningCoding = true //Boolean
+var currency = "$" //Char
+
+console.log("Student name is:" + " " + student1)
+console.info("Student score is:" + score + "\n" + "And learning speed is:" + learningSpeed)
+
+
+//Array //when different stuff we refer to it as a list, when the same we refer to it as an array
+var students = [ 
+    "Mike", //Index 0!!
+    "John",
+    "Mary",
+    100,
+    100.2,
+    true] 
+
+console.log(students[0] + "\n" + students[1])
